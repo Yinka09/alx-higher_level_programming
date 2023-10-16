62cd99fb70b6b5d852e8d2e9e6181a3b23165893
Yinka09 <kadirimaranatha@yahoo.com>
2023-10-16 14:50:05 -0700

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,7 +30,7 @@ class Square(Rectangle):
     def __str__(self):
         """module string represation of square
         """
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+        return "[Square] ({}) {}/{} - {}".format(self.id,
                                                          self.x,
                                                          self.y,
                                                          self.width)
