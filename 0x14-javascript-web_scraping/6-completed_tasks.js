685b7778adcd3b383babeb0a738b76478aa911a0
Yinka09 <kadirimaranatha@yahoo.com>
2024-01-31 06:16:36 -0800

    Changes made

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,21 +4,21 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
-	if (error) {
-		console.error(error);
-		return;
-	}
-	const todos = JSON.parse(body);
-	const completedTasks = {};
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const todos = JSON.parse(body);
+  const completedTasks = {};
 
-	todos.forEach(todo => {
-		if (todo.completed) {
-			if (completedTasks[todo.userId]) {
-				completedTasks[todo.userId]++;
-			} else {
-				completedTasks[todo.userId] = 1;
-			}
-		}
-	});
-	console.log(completedTasks);
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (completedTasks[todo.userId]) {
+        completedTasks[todo.userId]++;
+      } else {
+        completedTasks[todo.userId] = 1;
+      }
+    }
+  });
+  console.log(completedTasks);
 });
